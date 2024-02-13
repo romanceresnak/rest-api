ffe8a93ac4bb54f406706edeeef9bf009b8a6e71
romanceresnak <romanceresnak@windowslive.com>
2024-02-13 14:47:59 +0100

    dynamo construct

---
+++ lib/constructors/dynamodb/index.ts
@@ -0,0 +1,48 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as kms from 'aws-cdk-lib/aws-kms'
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+
+export interface ConstructorNameProps {
+    tableName: string;
+}
+
+export class DynamoDbConstructor extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: ConstructorNameProps) {
+    super(scope, id);
+
+    //Create a KMS for Key for DynamoDB table encryption
+    const tableKey = new kms.Key(this, 'TableKey', {
+        description: 'KMS key for encrypting the ${props.tableName} table`',
+        enableKeyRotation: true
+    })
+
+    // Define the DynamoDB table with a global secondary index
+    const table = new dynamodb.Table(this, 'MyTable', {
+        tableName: props.tableName,
+        partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+        billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+        readCapacity: 20,
+        writeCapacity: 20,
+        encryption: dynamodb.TableEncryption.CUSTOMER_MANAGED,
+        encryptionKey: tableKey,
+        pointInTimeRecovery: true,
+        tableClass: dynamodb.TableClass.STANDARD_INFREQUENT_ACCESS
+    })
+
+    // Define a global secondary index
+    table.addGlobalSecondaryIndex({
+        indexName: 'GSI1',
+        partitionKey: { name: 'gsi1pk', type: dynamodb.AttributeType.STRING}, 
+        sortKey: { name: 'gsi1sk', type: dynamodb.AttributeType.STRING },
+        projectionType: dynamodb.ProjectionType.ALL
+    })
+
+    // Add a Local Secondary Index
+    table.addLocalSecondaryIndex({
+        indexName: 'LSI1',
+        sortKey: { name: 'lsi1sk', type: dynamodb.AttributeType.STRING },
+        projectionType: dynamodb.ProjectionType.ALL,
+      });
+  }
+}
--- lib/rest-api-stack.ts
+++ lib/rest-api-stack.ts
@@ -1,16 +1,19 @@
 import * as cdk from 'aws-cdk-lib';
+import { RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { Table } from 'aws-cdk-lib/aws-dynamodb';
+import { Function } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class RestApiStack extends cdk.Stack {
+
+  private readonly applicationName: string;
+  private productTable: Table;
+  private productApiLambda: Function;
+  private productAPI: RestApi;
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'RestApiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
